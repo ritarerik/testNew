4d8242839361c025fd853c655d9e6b526cca8157
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-12-12 15:11:25 +0300

    3

--- src/KMP.java
+++ src/KMP.java
@@ -2,41 +2,27 @@ import java.util.Vector;
 
 public class KMP {
 
-	public static Vector<Integer> run(String S,  String T) {
-		
+	public static Vector<Integer> run(String S,  String T) {		
 		int pref[] = prefixFunction(T + "В¶" + S);		
-		Vector<Integer> res = new Vector<>();
-		
+		Vector<Integer> res = new Vector<>();		
 		for (int i = 0; i < pref.length; i++) {
 			if (pref[i] == T.length()) res.add(i - 2 * T.length()); 
-		}
-		
-		if (res.isEmpty()) res.add(-1);
-		
+		}		
+		if (res.isEmpty()) res.add(-1);		
 		return res;
 		
 	}
 	
-	public static int[] prefixFunction(String S) {
-		
+	public static int[] prefixFunction(String S) {		
 		int res[] = new int[S.length()];
-		for (int i : res) i = 0; 
-		
-		for (int i = 1; i < S.length(); i++) {
-			
-			int j = res[i - 1];
-			
-			while ((j > 0) && (S.charAt(i) != S.charAt(j))) j = res[j - 1];
-			
-			if (S.charAt(i) == S.charAt(j)) j++;
-			
-			res[i] = j;
-			
-		}	
-		
-		return res;
-		
-	
+		for (int i : res) i = 0; 		
+		for (int i = 1; i < S.length(); i++) {			
+			int j = res[i - 1];			
+			while ((j > 0) && (S.charAt(i) != S.charAt(j))) j = res[j - 1];			
+			if (S.charAt(i) == S.charAt(j)) j++;			
+			res[i] = j;			
+		}			
+		return res;	
 	}
 	
 }
--- src/ZFunction.java
+++ src/ZFunction.java
@@ -1,25 +1,17 @@
 public class ZFunction {
 
-	public static int[] run(String S) {
-		
+	public static int[] run(String S) {		
 		int n = S.length();
-		int Z[] = new int[n];
-		
-		for (int i = 1, l = 0, r = 0; i < n; ++i) {
-			
-			if (i <= r)	Z[i] = Math.min(r - i + 1, Z[i - l]);			
-			
-			while (i + Z[i] < n && S.charAt(Z[i]) == S.charAt(i+Z[i])) ++Z[i];
-			
+		int Z[] = new int[n];		
+		for (int i = 1, l = 0, r = 0; i < n; ++i) {			
+			if (i <= r)	Z[i] = Math.min(r - i + 1, Z[i - l]);				
+			while (i + Z[i] < n && S.charAt(Z[i]) == S.charAt(i+Z[i])) ++Z[i];			
 			if (i + Z[i] - 1 > r) {
 				l = i;  
 				r = i + Z[i] - 1;
 			}
-
-		}
-		
-		return Z;
-		
+		}		
+		return Z;		
 	}
 	
 }
