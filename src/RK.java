c00b88df86f5d41309f3c5aee30ee35935c7e580
metelevamd <metelevamd@KM-DIT-N01.doronichi.com>
2018-12-12 15:09:46 +0300

    2

--- src/RK.java
+++ src/RK.java
@@ -1,38 +1,33 @@
 import java.util.Vector;
 
-// Rabin–Karp algorithm
-
 public class RK {
 
 	public static Vector<Integer> run(String S, String T) {
 		
 		Vector<Integer> res = new Vector<>();
 		
-		int len = T.length(); 							// длина искомой подстроки 
+		int len = T.length();
 		
 		String text = "";
-		int textHash = 0; 								// хэш подстроки исходной строки
-		int subHash = getHash(S, T);					// хэш искомой строки	
+		int textHash = 0; 
+		int subHash = getHash(S, T);	
 		
 		for (int i = -1; i < S.length() - len; i++) {
 			
 			text = S.substring(i + 1, i + len + 1);		
 			textHash = getHash(S, text);
 			
-			// если хеши совпали, то посимвольное сравнение строк
 			if (subHash == textHash) {				
 				boolean equal = true;
 				if (!T.equals(text)) {
 					equal = false;
 					break;
 				}
-				// если посимвольно подстроки совпали, то добавляем позицию в массив
 				if (equal) res.add(i + 1);				
 			}
 
 		}
 		
-		// если совпадений не найдено, то возращение позиции -1
 		if (res.isEmpty()) res.add(-1);
 		
 		return res;
@@ -41,10 +36,9 @@ public class RK {
 	
 	private static int getHash(String S, String sub) {
 		
-		int q = 65713; // простое число
+		int q = 65713; 
 		int hash = 0;		
 		
-		// вычисление полиномиального хеша
 		for (int i = 0; i < sub.length(); i++) {			
 			hash += (((int) sub.charAt(i)) * Math.pow(2, sub.length() - i - 1)) % q;			
 		}
